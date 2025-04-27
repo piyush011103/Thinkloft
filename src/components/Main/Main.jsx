d30649d49a02aa44361899c8af399508957132c3
Piyush Kumar <pk8500454@gmail.com>
2025-04-27 17:27:58 +0530

    first commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
   return (
     <div className='main'>
       <div className="nav">
-        <p>Gemini</p>
+        <p>Thinkloft</p>
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
